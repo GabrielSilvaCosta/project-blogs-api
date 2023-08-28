d072c03d7c5075070f924b4bc1941bb9b6bc7c0e
GabrielSilvaCosta <bob@>
2023-08-28 10:13:20 -0300

    requisito 7 feito

---
+++ src/models/Category.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define(
+    "Category",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "categories",
+      underscored: true,
+      timestamps: false,
+    }
+  );
+
+  return Category;
+};
